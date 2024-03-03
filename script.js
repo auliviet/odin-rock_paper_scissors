6497f8e63e5ed0abde218c5708d4a4c69c3a9ab1
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-03-03 20:10:56 +1100

    Create functions to get player and computer choices

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-console.log("Hello from an external file");
+const ROCK = 0;
+const PAPER = 1;
+const SCISSORS = 2;
+
+function getComputerChoice () {
+    // Return a random int between 0 and 2
+    let randInt = Math.floor(Math.random() * (2 - 0 + 1) + 0);
+
+    switch (randInt) {
+        case 1:
+            return PAPER;
+            break;
+        
+        case 2:
+            return SCISSORS; 
+            break;
+    
+        default:
+            return ROCK;
+            break;
+    }
+}
+
+function getPlayerChoice () {
+    let playerChoice = prompt("Select Rock, Paper or Scissors:");
+    
+    playerChoice = playerChoice.toLowerCase();
+
+    if (playerChoice != "rock" && 
+        playerChoice != "paper" &&
+        playerChoice != "scissors") {
+            getPlayerChoice();
+        }
+    
+    if (playerChoice == "rock") {
+        return ROCK;
+    }
+    else if (playerChoice == "paper") {
+        return PAPER;
+    }
+    else {
+        return SCISSORS;
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "tie";
+    }    
+    
+    else if (playerSelection)
+}
+
+const computerSelection = getComputerChoice(); 
+const playerSelection = getPlayerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
+
+
